00410ade565debd53e837248427a0db97b6c0957
tosha-tosha <piletskaya.tonya@gmail.com>
2021-03-13 15:56:55 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Layout from './components/Layout';
+import bgImage2 from './assets/bg2.jpg';
+import bgImage3 from "./assets/bg3.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const App = () => {
+    return (
+        <>
+            <Header
+                title="Первый сайт на React.js"
+                descr="домашняя работа"
+            />
+            <Layout
+                id={1}
+
+                title="Абрикосы"
+                descr="что-то из фруктов"
+                bgImage={bgImage2}
+
+            />
+            <Layout
+                id={2}
+                title="Персики"
+                descr="что-то из других фруктов"
+                colorBg="pink"
+            />
+            <Layout
+                id={3}
+                title="Третий layout"
+                descr="передать картинку"
+                bgImage={bgImage3}
+            />
+            <Footer/>
+        </>
+    );
 }
+
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import s from './style.module.css';
+
+const Header = ({ title, descr }) => {
+    return (
+        <header className={s.root}>
+            <div className={s.forest}></div>
+            <div className={s.container}>
+                {/* Выводим пропсы title и descr с проверкой на пустоту */}
+                {title && <h1>{title}</h1>}
+                {descr && <p>{descr}</p>}
+            </div>
+
+        </header>
+    );
+};
+
+export default Header;
+
